2760f335a491eec2054059f44bc2e64d54029a94
Douglas Jasper <doug.jasper@trios.com>
2025-05-20 10:43:23 -0400

    Added header and footer

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <html>
     <head>
-
+        <title>Contact Manager - Home</title>
+        <link rel="stylesheet" type="txt/css" href="css/main.css" />
     </head>
     <body>
+        <?php include("header.php"); ?>
+
+        <main>
+            <h2>Contact List</h2>
+        </main>
+
+        <?php include("footer.php"); ?>
     </body>
 </html>
