08c77b0d486e675bcfb4ded83182e4f9326dfd30
Douglas Jasper <doug.jasper@trios.com>
2025-06-04 08:23:15 -0400

    Type Update Works

--- update_contact_form.php
+++ update_contact_form.php
@@ -1,79 +1,77 @@
 <?php
-    require_once('database.php');
-    // get the data from the form
-    $contact_id = filter_input(INPUT_POST, 'contact_id', FILTER_VALIDATE_INT);
+require_once('database.php');
 
-    // select the contact from the database
-    $query = 'SELECT * FROM contacts WHERE contactID = :contact_id';
+$contact_id = filter_input(INPUT_POST, 'contact_id', FILTER_VALIDATE_INT);
 
-        $statement = $db->prepare($query);
-        $statement->bindValue(':contact_id', $contact_id);        
+// Get the contact's current data
+$query = 'SELECT * FROM contacts WHERE contactID = :contact_id';
+$statement = $db->prepare($query);
+$statement->bindValue(':contact_id', $contact_id);
+$statement->execute();
+$contact = $statement->fetch();
+$statement->closeCursor();
 
-        $statement->execute();
-        $contact = $statement->fetch();
-        $statement->closeCursor();
+// Get all contact types
+$queryTypes = 'SELECT * FROM types';
+$statement2 = $db->prepare($queryTypes);
+$statement2->execute();
+$contact_types = $statement2->fetchAll();
+$statement2->closeCursor();
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Contact Manager - Update Contact</title>
-        <link rel="stylesheet" type="text/css" href="css/main.css" />
-    </head>
-    <body>
-        <?php include("header.php"); ?>
+<head>
+    <title>Contact Manager - Update Contact</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+</head>
+<body>
+    <?php include("header.php"); ?>
 
-        <main>
-            <h2>Update Contact</h2>
+    <main>
+        <h2>Update Contact</h2>
 
-            <form action="update_contact.php" method="post" id="update_contact_form"
-                enctype="multipart/form-data">
+        <form action="update_contact.php" method="post" id="update_contact_form" enctype="multipart/form-data">
+            <div id="data">
+                <input type="hidden" name="contact_id" value="<?php echo $contact['contactID']; ?>" />
 
-                <div id="data">
+                <label>First Name:</label>
+                <input type="text" name="first_name" value="<?php echo $contact['firstName']; ?>" /><br />
 
-                    <input type="hidden" name="contact_id"
-                        value="<?php echo $contact['contactID']; ?>" />
+                <label>Last Name:</label>
+                <input type="text" name="last_name" value="<?php echo $contact['lastName']; ?>" /><br />
 
-                    <label>First Name:</label>
-                    <input type="text" name="first_name"
-                        value="<?php echo $contact['firstName']; ?>" /><br />
+                <label>Email Address:</label>
+                <input type="text" name="email_address" value="<?php echo $contact['emailAddress']; ?>" /><br />
 
-                    <label>Last Name:</label>
-                    <input type="text" name="last_name"
-                        value="<?php echo $contact['lastName']; ?>" /><br />
+                <label>Phone Number:</label>
+                <input type="text" name="phone_number" value="<?php echo $contact['phone']; ?>" /><br />
 
-                    <label>Email Address:</label>
-                    <input type="text" name="email_address"
-                        value="<?php echo $contact['emailAddress']; ?>" /><br />
+                <label>Status:</label>
+                <input type="radio" name="status" value="member" <?php echo ($contact['status'] == 'member') ? 'checked' : ''; ?> />Member<br />
+                <input type="radio" name="status" value="nonmember" <?php echo ($contact['status'] == 'nonmember') ? 'checked' : ''; ?> />Non-Member<br />
 
-                    <label>Phone Number:</label>
-                    <input type="text" name="phone_number"
-                        value="<?php echo $contact['phone']; ?>" /><br />
+                <label>Birth Date:</label>
+                <input type="date" name="dob" value="<?php echo $contact['dob']; ?>" /><br />
 
-                    <label>Status:</label>
-                    <input type="radio" name="status" value="member"
-                        <?php echo ($contact['status'] == 'member') ? 'checked' : ''; ?> />Member<br />
-                    <input type="radio" name="status" value="nonmember"
-                        <?php echo ($contact['status'] == 'nonmember') ? 'checked' : ''; ?> />Non-Member<br />
+                <label>Contact Type:</label>
+                <select name="type_id">
+                    <?php foreach ($contact_types as $type): ?>
+                        <option value="<?php echo $type['typeID']; ?>" <?php if ($type['typeID'] == $contact['typeID']) echo 'selected'; ?>>
+                            <?php echo htmlspecialchars($type['contactType']); ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select><br />
+            </div>
 
-                    <label>Birth Date:</label>
-                    <input type="date" name="dob"
-                        value="<?php echo $contact['dob']; ?>" /><br />
+            <div id="buttons">
+                <label>&nbsp;</label>
+                <input type="submit" value="Update Contact" /><br />
+            </div>
+        </form>
 
-                </div>
+        <p><a href="index.php">View Contact List</a></p>
+    </main>
 
-                <div id="buttons">
-
-                    <label>&nbsp;</label>
-                    <input type="submit" value="Update Contact" /><br />
-
-                </div>
-
-            </form>
-
-            <p><a href="index.php">View Contact List</a></p>
-            
-        </main>
-
-        <?php include("footer.php"); ?>
-    </body>
+    <?php include("footer.php"); ?>
+</body>
 </html>
